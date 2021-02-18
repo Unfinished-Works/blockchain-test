fbec710839b1fcc3cc79295d9c1e46f8d6268468
lemon-mint <55233766+lemon-mint@users.noreply.github.com>
2021-02-18 13:20:20 +0900

    PoW

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.15
 
 require (
 	github.com/zeebo/blake3 v0.1.0
+	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2
 	google.golang.org/protobuf v1.25.0
 )
--- pow/pow.go
+++ pow/pow.go
@@ -1,1 +1,22 @@
 package pow
+
+import (
+	"encoding/binary"
+
+	"github.com/zeebo/blake3"
+	"golang.org/x/crypto/argon2"
+)
+
+//HashFunc : PoW Hash Function
+func HashFunc(data []byte, nonce uint64) []byte {
+	h := blake3.New()
+	h.Write(data)
+	data = h.Sum(nil)
+	buf := make([]byte, 8)
+	binary.BigEndian.PutUint64(buf, nonce)
+	h.Reset()
+	h.Write(buf)
+	h.Write(data)
+	buf = h.Sum(nil)[:16]
+	return argon2.IDKey(data, buf, 1, 64*1024, 4, 32)
+}
